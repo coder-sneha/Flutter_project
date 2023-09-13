57245b11c1bc4a143db388418c2be9f6cff3aebc
coder-sneha <sneha.nkhs@gmail.com>
2023-09-13 22:29:57 +0530

    Tic Tac Toe Game App

---
+++ tic_tac_toe_app/lib/constants/colors.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+class MainColor {
+  static Color primaryColor = Color.fromARGB(255, 17, 230, 212);
+  static Color secondaryColor = Color.fromARGB(255, 126, 53, 86);
+  static Color accentColor = Color.fromARGB(255, 166, 180, 12);
+}
